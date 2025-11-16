0408ea2b3b74d45b525f3ed0d0ce3987ef90ffca
Thirumal <m.thirumal@hotmail.com>
2025-11-16 14:19:00 +0530

    first commit

---
+++ src/main/java/io/github/thirumalx/CertxApplication.java
@@ -0,0 +1,13 @@
+package io.github.thirumalx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertxApplication.class, args);
+	}
+
+}
